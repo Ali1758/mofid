e85c082125c83e02e09421fa031d6ea7d13faa63
ali <ali.rasoulinejad58@gmail.com>
2019-08-05 18:39:28 -0700

    Add User admin class

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('first_name', 'last_name', 'telegram_user', 'accessibility')
+    actions = ['access_accept']
+
+    def access_accept(self, request, queryset):
+        queryset.update(accessibility=True)
+    access_accept.short_description = 'Accept accessibility of selected Users.'
