2f00a25581c1eb78ccd1c111c7565e8263c8bc3e
ali <ali.rasoulinejad58@gmail.com>
2019-08-05 18:37:57 -0700

    edit User model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    Accessibility = models.BooleanField()
+    accessibility = models.BooleanField(default=False)
+    telegram_user = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.first_name, self.last_name)
+
