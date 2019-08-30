b39671fab9004a1143d7ee2cbda2559eefc18e08
ali <ali.rasoulinejad58@gmail.com>
2019-08-30 17:44:49 +0430

    Create custom manager for model

--- users/models.py
+++ users/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class UserManager(models.Manager):
+    def get_queryset(self):
+        return super(UserManager, self).get_queryset().filter(accessibility=True)
+
+
 class User(AbstractUser):
     accessibility = models.BooleanField(default=False)
     telegram_id = models.IntegerField(max_length=15)
