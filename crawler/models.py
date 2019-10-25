472d0c05727a5d3fd1d414e31007dd8f4b6dec51
ali <ali.rasoulinejad58@gmail.com>
2019-10-25 22:37:29 +0330

    Create Storage model for storing the outputs

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+
+class Storage(models.Model):
+    name = models.CharField(max_length=25)
+    complete = models.BooleanField(default=False)
+    start = models.DateTimeField(auto_created=True)
+    final = models.DateTimeField(auto_now=True)
+    address = models.FilePathField(path=settings.MEDIA_ROOT)
+
+    class Meta:
+        ordering = ('-start',)
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.start)
+
+    def download_link(self):
+        reverse("download", args=[self.name])
