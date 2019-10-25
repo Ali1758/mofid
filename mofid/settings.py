b6c802e3a500cf8ea40f67989ff0901f6acd316d
ali <ali.rasoulinejad58@gmail.com>
2019-10-25 20:54:55 +0330

    Add authenticate url

--- mofid/settings.py
+++ mofid/settings.py
@@ -118,7 +118,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 AUTH_USER_MODEL = 'users.User'
-
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'Login'
 
 # Celery application definition
 CELERY_BROKER_URL = 'redis://localhost:6379'
--- mofid/urls.py
+++ mofid/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from telegram.views import my_view, IndexView
-
 # from django.conf import settings
 # from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('test/', view),
     path('', IndexView.as_view(), name='index'),
+    path('login', LoginView.as_view(), name='Login'),
+    path('logout', LogoutView.as_view(), name='Logout')
 ]
 
 # if settings.DEBUG:
