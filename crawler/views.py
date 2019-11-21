8799b401de4ea52927cc83c67f1c2147e128a940
Ali <ali.rasoulinejad58@gmail.com>
2019-11-21 19:39:40 +0330

    Change form and combine 2 crawling conditions for simplicity

--- crawler/views.py
+++ crawler/views.py
@@ -3,10 +3,6 @@ from django.http.response import HttpResponse
 from .models import Storage
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .crawler import crawler_engine
-from jdatetime import datetime
-from django.views.generic.list import ListView
 
 
 @login_required()
@@ -22,19 +18,3 @@ def download_items(request, slug):
     response = HttpResponse(file, content_type='application/vnd.ms-excel')
     response['Content-Disposition'] = "attachment; filename={}".format(output.name)
     return response
-
-
-@login_required()
-def full_crawling(request):
-    output_name = "Output_{}".format(str(datetime.now()).split('.')[-2])
-    output_path = output_name + '.xlsx'
-    s = Storage(name=output_name, complete=False, address=output_path)
-    s.save()
-    crawler_engine.delay(output_name)
-    messages.success(request, "فرآیند آغاز شد")
-    return redirect('index')
-
-
-@login_required()
-def custom_crawling(request):
-    return None
--- mofid/views.py
+++ mofid/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from crawler.models import Storage
-from crawler.forms import SelectCrawlingModelForm
+from crawler.forms import CustomForm
 from django.shortcuts import redirect
+from datetime import datetime
+from crawler.models import Storage
+from crawler.crawler import crawler_engine
+from django.contrib import messages
 
 
-def index_view(requests):
+def index_view(request):
     last5output = Storage.objects.all().filter(complete=True)[:5]
-    if requests.POST:
-        form = SelectCrawlingModelForm(requests.POST)
+    if request.POST:
+        form = CustomForm(request.POST)
         if form.is_valid():
-            option = form.cleaned_data['option']
-            if option == 'custom':
-                return redirect('custom_crawling')
-            elif option == 'all':
-                return redirect('full_crawling')
+            data = form.cleaned_data
+            name = "Output_{}".format(str(datetime.now()).split('.')[-2])
+            path = name + '.xlsx'
+            s = Storage(name=name, complete=False, address=path, starter=request.user, type='All')
+            s.save()
+            crawler_engine.delay(outputname=name, sites=data.sites, users=data["users"])
+            messages.success(request, "فرآیند آغاز شد")
+            return redirect('index')
     else:
-        form = SelectCrawlingModelForm()
-    return render(requests, template_name='index.html', context={'items': last5output, 'form': form})
+        form = CustomForm()
+    return render(request, template_name='index.html', context={'items': last5output, 'form': form})
