1e9c44c5e00f2b25d642c10b9a6e0ba9326245d8
Ali <ali.rasoulinejad58@gmail.com>
2019-11-21 19:40:38 +0330

    Create CustomForm crawling

--- crawler/forms.py
+++ crawler/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from users.models import User
 
 
 class SelectCrawlingModelForm(forms.Form):
@@ -6,3 +7,28 @@ class SelectCrawlingModelForm(forms.Form):
                                label='جهت شروع فرآیند، لطفا یکی از گزینه‌های زیر را انتخاب نمایید:',
                                choices=(('all', 'همه محصولات'), ('custom', 'تنظیمات اختیاری')),
                                widget=forms.RadioSelect())
+
+
+class CustomForm(forms.Form):
+    SelectList = (("mofidteb", "مفیدطب"), ("darukade", "داروکده"), ("mosbatesabz", "مثبت سبز"),
+                  ("digikala", "دیجیکالا"), ("ezdaru", "ایزی دارو"), ("shider", "شیدر"))
+
+    users = forms.MultipleChoiceField(label="ارسال اعلانات برای افراد منتخب",
+                                      required=True,
+                                      initial=["ali"],
+                                      choices=((user.username, user.get_full_name()) for user in User.access.all()),
+                                      error_messages={'required': 'حداقل یکی از کاربران باید انتخاب گردد'},
+                                      widget=forms.CheckboxSelectMultiple)
+
+    sites = forms.MultipleChoiceField(label="سایت های مورد نظر",
+                                      required=True,
+                                      initial=[site[0] for site in SelectList],
+                                      choices=SelectList,
+                                      error_messages={'required': 'حداقل یکی از سایت های موردنظر باید انتخاب گردد'},
+                                      widget=forms.CheckboxSelectMultiple)
+
+    def clean_users(self):
+        users = self.cleaned_data['users']
+        if "ali" not in users:
+            raise forms.ValidationError("عدم انتخاب کاربر ادمین")
+        return users
