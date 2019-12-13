cb52bceb68d545c9cb90408b7f3f97a76181f4c1
Ali <ali.rasoulinejad58@gmail.com>
2019-12-13 12:27:56 +0330

    Update setting for upload on server

--- mofid/settings.py
+++ mofid/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9s12bj_ybsd1y2@(2#frxx#bu=j#gw)e90!h!@qo02vc5%s_^z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["85.185.93.78", "127.0.0.1"]
 
 
 # Application definition
