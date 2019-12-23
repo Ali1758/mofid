b72de9559f5cef41cc993c90cb024f9a5686505f
Ali <ali.rasoulinejad58@gmail.com>
2019-12-23 23:19:22 +0330

    Update admin storage: adding backups inline

--- crawler/admin.py
+++ crawler/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import Storage, Backup
 
 
+class BackupInline(admin.TabularInline):
+    model = Backup
+    exclude = ('created',)
+
+
 @admin.register(Storage)
 class StorageAdmin(admin.ModelAdmin):
     list_display = ('name', 'start', 'complete')
+    inlines = [BackupInline]
 
 
 admin.site.register(Backup)
