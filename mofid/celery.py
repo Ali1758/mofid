d73ebc05365b452e55a188ec1ba9bbd0bb60e5a5
Ali <ali.rasoulinejad58@gmail.com>
2020-09-27 19:13:59 +0330

    fix celery config error

--- mofid/celery.py
+++ mofid/celery.py
@@ -7,7 +7,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mofid.settings')
 
 app = Celery('mofid')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
